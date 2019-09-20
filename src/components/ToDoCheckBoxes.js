e2e7621b7bafb2780898634bd808608923a02b6c
George <george.emad@outlook.com>
2019-09-20 18:11:43 +0200

    remove unwanted comments

--- src/components/ToDoCheckBoxes.js
+++ src/components/ToDoCheckBoxes.js
@@ -11,13 +11,7 @@ function CheckBoxes(props){
             name="checkbox"
             id="checkbox1"
             value="value" 
-            /* i called the method handleChange, which is passed into the CheckBoxes component tag
-            handleChange={this.handleChange}
-            the function inside onChange ==> is the function receiving the event what it does it called props.handleChange
-            */
             onChange={()=> props.handleChange(props.all.id)}
-            /* the reason the checkboxes working ( check and uncheck ) is because of this below checked property
-            checked={} determines whether the checkbox should be checked or not  */
             checked={props.all.completed}/>
 
             <span>{props.all.text}</span>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-/* App */
-/* import App from './App';
-ReactDOM.render(<App />, document.getElementById('root')); */
-
 /* Class State lessons  */
 import ClassStateApp from "./ClassStateApp"
 ReactDOM.render(<ClassStateApp />, document.getElementById('root'))
